381828c0ec5abdef5668e40e068bdf2566426ccb
이기준 <2stndard@gmail.com>
2021-01-23 20:13:09 +0900

    21-01-23-2

--- Exploratory time series data analysis.R
+++ Exploratory time series data analysis.R
@@ -138,7 +138,130 @@ cycle(AirPassengers)
 # press
 # 4   -> answer
 
+# Exercise
+# Missing values
+# Sometimes there are missing values in time series data, denoted NA in R, and it is useful to know their locations. It is also important to know how missing values are handled by various R functions. Sometimes we may want to ignore any missingness, but other times we may wish to impute or estimate the missing values.
+# 
+# Let's again consider the monthly AirPassengers dataset, but now the data for the year 1956 are missing. In this exercise, you'll explore the implications of this missing data and impute some new data to solve the problem.
+# 
+# The mean() function calculates the sample mean, but it fails in the presence of any NA values. Use mean(___, na.rm = TRUE) to calculate the mean with all missing values removed. It is common to replace missing values with the mean of the observed values. Does this simple data imputation scheme appear adequate when applied the the AirPassengers dataset?
+#   
+#   Instructions
+# 70 XP
+# Instructions
+# 70 XP
+# Use plot() to display a simple plot of AirPassengers. Note the missing data for 1956.
+# Use mean() to calculate the sample mean of AirPassengers with the missing data removed (na.rm = TRUE).
+# Run the pre-written code to impute the mean values into your missing data.
+# Use another call to plot() to replot your newly imputed AirPassengers data.
+# Run the pre-written code to add the complete AirPassengers data to your plot.
 
 
+# Plot the AirPassengers data
+plot(AirPassengers)
+
+# Compute the mean of AirPassengers
+mean(AirPassengers, na.rm = TRUE)
+
+# Impute mean values to NA in AirPassengers
+AirPassengers[85:96] <- mean(AirPassengers, na.rm = TRUE)
+
+# Generate another plot of AirPassengers
+plot(AirPassengers)
+
+# Add the complete AirPassengers data to your plot
+rm(AirPassengers)
+points(AirPassengers, type = "l", col = 2, lty = 3)
+
+
+# Exercise
+# Creating a time series object with ts()
+# The function ts() can be applied to create time series objects. A time series object is a vector (univariate) or matrix (multivariate) with additional attributes, including time indices for each observation, the sampling frequency and time increment between observations, and the cycle length for periodic data. Such objects are of the ts class, and represent data that has been observed at (approximately) equally spaced time points. Now you will create time series objects yourself.
+# 
+# The advantage of creating and working with time series objects of the ts class is that many methods are available for utilizing time series attributes, such as time index information. For example, as you've seen in earlier exercises, calling plot() on a ts object will automatically generate a plot over time.
+# 
+# In this exercise, you'll familiarize yourself with the ts class by encoding some time series data (saved as data_vector) into ts and exploring the result. Your time series data_vector starts in the year 2004 and has 4 observations per year (i.e. it is quarterly data).
+# 
+# Instructions
+# 100 XP
+# Instructions
+# 100 XP
+# Apply print() and plot() to data_vector. Note that, by default, your plot does not contain time information.
+# Use ts() with data_vector to convert your data to a ts object. Set the start argument equal to 2004 and the frequency argument equal to 4. Assign the result to time_series.
+# Use print() and plot() to view your time_series object.
+
+data_vector <- c(2.05219410731111,4.29288527974091,3.32941329441844,3.50859506696045,0.000957693762193124,1.92171863450249,0.797813412800535,0.299954343478063,0.943568753562609,0.574828338758714,-0.00340059028674611,0.34486491757086,2.22297611360904,0.1763144576315,2.70976227698024,1.25019489653419,-0.400716475448651,0.88527321208965,-1.5852420265539,-2.28292788910447,-2.56095312898444,-3.12599637544793,-2.86602958954692,-1.78470092068662,-1.88949129078692,-2.7255351193826,-2.10331418003627,-0.0174256893338487,-0.361320415116881,-2.90084033266105,-3.28474409267047,-2.8684594717709,-1.95050744365674,-4.88018925247303,-3.2634605352894,-1.63960625222554,-3.30125758402894,-2.63312454334943,-1.70583540221139,-2.21198250613255,-0.517059518572955,0.0752508094841423,-0.840699471563585,-1.40226834874561,-0.138211423023857,-1.40659547028281,-2.3046941054804,1.50738914324565,0.711867947688864,-1.13005190223045)
+
+# Use print() and plot() to view data_vector
+print(data_vector)
+plot(data_vector)
+
+# Convert data_vector to a ts object with start = 2004 and frequency = 4
+time_series <- ts(data_vector, start = 2004, frequency = 4)
+
+# Use print() and plot() to view time_series
+print(time_series)
+plot(time_series)
+
+
+# Exercise
+# Testing whether an object is a time series
+# When you work to create your own datasets, you can build them as ts objects. Recall the dataset data_vector from the previous exercise, which was just a vector of numbers, and time_series, the ts object you created from data_vector using the ts() function and information regarding the start time and the observation frequency. As a reminder, data_vector and time_series are shown in the plot on the right.
+# 
+# When you use datasets from others, such as those included in an R package, you can check whether they are ts objects using the is.ts() command. The result of the test is either TRUE when the data is of the ts class, or FALSE if it is not.
+# 
+# In this exercise, you'll explore the class of the datasets you've been using throughout this chapter.
+# 
+# Instructions
+# 100 XP
+# Use is.ts() on the data_vector and time_series objects from the previous exercise.
+# Use another call to is.ts() to check the class of the Nile dataset used in earlier exercises.
+# Continue the trend by using is.ts() on the AirPassengers dataset.  
+
+
+# Check whether data_vector and time_series are ts objects
+is.ts(data_vector)
+is.ts(time_series)
+
+# Check whether Nile is a ts object
+is.ts(Nile)
+
+# Check whether AirPassengers is a ts object
+is.ts(AirPassengers)
+
+
+# Exercise
+# Plotting a time series object
+# It is often very useful to plot data we are analyzing, as is the case when conducting time series analysis. If the dataset under study is of the ts class, then the plot() function has methods that automatically incorporate time index information into a figure.
+# 
+# Let's consider the eu_stocks dataset (available in R by default as EuStockMarkets). This dataset contains daily closing prices of major European stock indices from 1991-1998, specifically, from Germany (DAX), Switzerland (SMI), France (CAC), and the UK (FTSE). The data were observed when the markets were open, so there are no observations on weekends and holidays. We will proceed with the approximation that this dataset has evenly spaced observations and is a four dimensional time series.
+# 
+# To conclude this chapter, this exercise asks you to apply several of the functions you've already learned to this new dataset.
+# 
+# Instructions
+# 100 XP
+# Use is.ts() to check whether eu_stocks is a ts object.
+# View the start, end, and frequency of eu_stocks using the start(), end(), and frequency() functions, respectively.
+# Generate a simple plot of your eu_stocks data using the plot() command.
+# Generate a more complex time series plot of your eu_stocks data using the ts.plot() command. Input the eu_stocks dataset into the pre-written code, but leave the other arguments as they are.
+# Use the pre-written code to add a legend to your time series plot.
+
+# Check whether eu_stocks is a ts object
+eu_stocks <- EuStockMarkets
+is.ts(eu_stocks)
+
+# View the start, end, and frequency of eu_stocks
+start(eu_stocks)
+end(eu_stocks)
+frequency(eu_stocks)
+
+# Generate a simple plot of eu_stocks
+plot(eu_stocks)
+
+# Use ts.plot with eu_stocks
+ts.plot(eu_stocks, col = 1:4, xlab = "Year", ylab = "Index Value", main = "Major European Stock Indices, 1991-1998")
+
+# Add a legend to your ts.plot
+legend("topleft", colnames(eu_stocks), lty = 1, col = 1:4, bty = "n")
 
 
